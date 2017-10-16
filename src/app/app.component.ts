c5f61350329bf1a9ff41f67bdef12fa7ebf675d7
subhashini <subhashini.parthasarathy@dnata.com>
2017-10-16 16:05:19 +0400

    latest changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent { 
+  serverElements = [{type:'server', name:'TestServer', content:'Just a Server'}];
+
+  onServerAdded(serverData:{serverName: string, serverContent: string}){
+     this.serverElements.push({
+       type : 'server',
+       name : serverData.serverName,
+       content : serverData.serverContent
+     });
+   }
+
 }
---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, EventEmitter,Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  newServerName = '';
+  newServerContent = '';
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddServer(){
+   this.serverCreated.emit({serverName: this.newServerName,serverContent: this.newServerContent});
+  }
+
+}
